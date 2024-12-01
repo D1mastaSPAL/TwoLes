3eeebea6df98197278869e563aa71e238eff5b94
D1mastaSPAL <su66071n@mail.ru>
2024-12-01 16:43:43 +0300

    first commit

---
+++ Lesson2.1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Lesson2._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя: ");
+            string name = Console.ReadLine();
+            double age, height;
+            Console.WriteLine("Введите возраст: ");
+            age = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите рост в сантиметрах: ");
+            height = Convert.ToDouble(Console.ReadLine()) / 100;
+            Console.WriteLine($"Привет, {name}! {"\n"} {"\t"} Ваш возраст: {age} лет. {"\n"} {"\t"} {"\t"} Ваш рост: {height} м.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson2.2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Lesson2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstValue, doubleValue;
+            Console.WriteLine("Введите число 1: ");
+            firstValue = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число 2: ");
+            doubleValue = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Сумма чисел: " + (firstValue + doubleValue));
+            Console.WriteLine("Разность чисел: " + (firstValue - doubleValue));
+            Console.WriteLine("Произведение чисел: " + (firstValue * doubleValue));
+            Console.WriteLine("Частное чисел: " + (firstValue / doubleValue));
+            Console.ReadKey();
+        }
+    }
+}
